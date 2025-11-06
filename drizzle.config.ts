1b55fa36afe1d7dbfe580d92d321589225616ab0
Patryk Kudlik <patryk.kudlik@gmail.com>
2025-11-06 10:01:32 +0100

    feat: integrate BetterAuth for user authentication and session management

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    out: './src/migrations',
-    schema: './src/db/schema/index.ts',
+    out: './src/drizzle/migrations',
+    schema: './src/drizzle/schema/index.ts',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
