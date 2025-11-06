48200941106e0abe97f0c9d5636d496edf5d92a8
Patryk Kudlik <patryk.kudlik@gmail.com>
2025-11-06 09:43:31 +0100

    feat: add database integration with Neon and Drizzle ORM

---
+++ src/drizzle/db.ts
@@ -0,0 +1,16 @@
+import { neon } from '@neondatabase/serverless';
+import { config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+import * as schema from './schema/index';
+
+config({
+    path: '../../.env',
+});
+
+const sql = neon(process.env.DATABASE_URL!);
+
+export const db = drizzle({
+    client: sql,
+    schema,
+});
