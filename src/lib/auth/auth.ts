8f0a058c3822e0d9f622e081d329383eeb1a58ac
Patryk Kudlik <patryk.kudlik@gmail.com>
2025-11-07 12:53:48 +0100

    refactor(auth): include full Drizzle schema in auth adapter config

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,4 +1,5 @@
 import { db } from '@drizzle/db';
+import * as schema from '@drizzle/schema';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { nextCookies } from 'better-auth/next-js';
@@ -6,6 +7,7 @@ import { nextCookies } from 'better-auth/next-js';
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: 'pg',
+        schema,
     }),
     appName: 'react-task-profile',
     emailAndPassword: {
