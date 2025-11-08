5a8be0558f55faece43f7a4e2f352e55619610d5
Patryk Kudlik <patryk.kudlik@gmail.com>
2025-11-08 17:19:01 +0100

    feat: protect page route by fetching session and redirect authenticated users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import Header from '@/components/header';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
+import Header from '@/components/header';
+import { auth } from '@/lib/auth/auth';
+
+export default async function Page() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (session?.user) redirect('/profile');
+
     return (
         <>
             <section aria-label="Seitenkopf mit Hauptnavigation">
